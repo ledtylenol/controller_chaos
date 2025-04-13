2b5b64541f76eff5c9e1b92671fef2310b5d3730
DotMouse <mousedotexe@gmail.com>
2025-04-13 19:37:53 +0200

    Prepared Pentagon Sprite

--- entities/player/sprite_face_animation.gd
+++ entities/player/sprite_face_animation.gd
@@ -7,3 +7,10 @@ extends Node
 func ouch() -> void:
 	anim.stop()
 	anim.play("ouch", -1, 1.0)
+
+func look_at( look_at_position:Vector2 ) -> void:
+	# Math I don't know how to do
+	
+	# Do animation
+	anim.stop()
+	anim.play("look_at", -1, 1.0)
